9d4077c05489761ec31346dae87dac2efbf75303
MS-Cinti <szabadicintia@gmail.com>
2022-04-04 16:16:36 +0200

    added new files

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,19 @@ const path = require('path');
 
 const port = 9000;
 const app = express();
+
 //next: ha itt végzett akkor hajtson e végre műveletet vagy sem
 app.get('/', (req, res, next) => { 
-    console.log('Request received.');
-    res.send('Thank you for your request! This is our response.')
+    //console.log('Request received.');
+    //res.send('Thank you for your request! This is our response.')
+    
+    //ezzel bármilyen létező filet kiszolgálhatunk, ezzel az index.html-el minden filet ki tudunk szolgálni
+    res.sendFile(path.join(`${__dirname}/../frontend/index.html`));
+})
+
+//példa gyakorlásnak: itt a someFile.json-t szolgáljuk ki a kismacska endpointon
+app.get('/kismacska', (req, res, next) => { 
+    res.sendFile(path.join(`${__dirname}/../frontend/someFile.json`));
 })
 
 app.get('/something', (req, res, next) => {
@@ -29,6 +38,9 @@ app.get('/api/v1/users', (req, res, next) => {
 })
 
 //minden request beérkezett ide a terminálba
+
+//listen elé kell ezt tenni, hogy ne akadjon össze
+app.use('/pub', express.static(`${__dirname}/../frontend/public`));
 
 app.listen(port, () => {
     console.log(`http://127.0.0.1:${port}`)
---
+++ frontend/public/script.js
@@ -0,0 +1,26 @@
+const parseJSON = async (url) => {
+    //változóba mentjük a fetchet
+    const response = await fetch(url);
+    return response.json();
+};
+
+//objectumból kibontjuk a kulcsait: ({name, surname})
+const userComponent = ({name, surname}) => {
+    return `
+        <div>
+            <h1>${name}</h1>
+            <h2>${surname}</h2>
+        </div>
+    `
+};
+
+const loadEvent = async () => {
+    const result = await parseJSON('/api/v1/users');
+    const rootElement = document.getElementById("root");
+    rootElement.insertAdjacentHTML(
+        "beforeend", 
+        result.map(user => userComponent(user)).join("")
+    );
+};
+
+window.addEventListener('load', loadEvent);
